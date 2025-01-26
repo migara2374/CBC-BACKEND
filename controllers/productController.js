d5ef7b5f3463526a2554c055f1d8a4fa6972adac
migara2374 <migaravimukthi69@gmail.com>
2025-01-27 01:23:30 +0530

    create product router

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,2 +1,25 @@
 import Product from "../models/product.js";
+import {isAdmin} from "./userController.js"
 
+export function createProduct(req,res){ 
+    if(!isAdmin(req)){
+        res.json({
+            message: "Please login as administrator to add products" 
+        })
+        return 
+    } 
+
+    const newProductData = req.body 
+
+    const product = new Product(newProductData) 
+
+    product.save().then(()=>{
+        res.join({
+            message: "Product created"
+        }).catch((error)=>{
+            res.json({
+                message: error
+            })
+        })
+    })
+} 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,93 +1,105 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv" ;
+import dotenv from "dotenv";
 
 dotenv.config()
 
-export function createUser(req,res){ 
+export function createUser(req,res){
+  const newUserdata = req.body 
 
-  const newUserData = req.body 
+  if(newUserdata.type == "admin"){
 
-  if(newUserData.type == "admin"){
-
-    if(req.user==null){
+    if(req.user == null){
       res.json({
-        message: "Please login as administrator to create admin accounts" 
-      }) 
+        message : "Please login as administrator to create admin accounts"
+      })
       return 
     } 
 
     if(req.user.type != "admin"){
-      res.json({
-        message: "Please login as administrator to create admin accounts"
+      res.join({
+        message: "Please login as administrator to create admin accounts" 
       })
       return 
     }
-  }
+  } 
 
-  newUserData.password = bcrypt.hashSync(newUserData.password, 10)  
+  newUserdata.password = bcrypt.hashSync 
+  (newUserdata.password, 10) 
 
-  const user = new User(newUserData) 
+  const user = new User(newUserdata) 
 
   user.save().then(()=>{
     res.json({
-      message: "User created"
+      message: "User Created" 
     })
   }).catch((error)=>{
-    res.json({      
-      message: "User not created"
+    res.json({
+      message: "User not created" 
     })
   })
-  
-}
+}  
+
 
 export function loginUser(req,res){
 
   User.find({email : req.body.email}).then(
     (users)=>{
       if(users.length == 0){
-
         res.json({
           message: "User not found"
-        })
+        }) 
+      } else {
+        const user = users[0] 
 
-      }else{
-
-        const user = users[0]
-
-        const isPasswordCorrect = bcrypt.compareSync(req.body.password,user.password)
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password) 
 
         if(isPasswordCorrect){
-
-          const token = jwt.sign({
+          const token = jwt.sign({ 
             email : user.email,
             firstName : user.firstName,
-            lastName : user.lastName,
+            lastName : user.lastName, 
             isBlocked : user.isBlocked,
             type : user.type,
             profilePicture : user.profilePicture
-          } , process.env.SECRET) 
-          
+          }, process.env.SECRET)
+
           res.json({
             message: "User logged in",
-            token: token
+            token: token 
           })
-          
         }else{
-          res.json({
-            message: "User not logged in (wrong password)"
-          })
+            res.json({
+              message: "User not logged in (wrong password)" 
+            })
         }
       }
     }
   )
-} 
+}
 
-export function deleteUser(req,res){
-    User.deleteOne({email : req.body.email}).then(()=>{
-         res.json({
-          message : "Student deleted" 
-         })
-    });
-}  
+
+export function isAdmin(req){
+  if(req.user==null){
+    return false 
+  } 
+  if(req.user.type != "admin"){
+    return false 
+  } 
+
+  return true 
+}
+
+
+export function isCustomer(req){
+  if(req.user==null){
+    return false 
+  } 
+
+  if(req.user.type != "customer"){
+    return false 
+  } 
+
+  return true 
+} 
